93ed7b658c961bbabe8b5cc111cd0e6d8a985530
unknown <rockph0029@gmail.com>
2024-04-12 14:30:34 +0800

    add task form

--- src/app/tasks/add-new/upcoming-task.tsx
+++ src/app/tasks/add-new/upcoming-task.tsx
@@ -4,7 +4,6 @@ import {
     Card,
     CardContent,
     CardFooter,
-    CardDescription,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
@@ -13,73 +12,52 @@ import { Progress } from '@/components/ui/progress';
 
 import { TbProgressCheck } from "react-icons/tb";
 
+const data = [{
+    title: 'Create anassssssrwarwarw UI',
+    description: 'Lorem Ipsum safdafafsaf',
+    progress: 25
+}, {
+    title: 'Create anassssssrwarwarw UI',
+    description: 'Lorem Ipsum safdafafsaf',
+    progress: 79
+}, {
+    title: 'Create anassssssrwarwarw UI',
+    description: 'Lorem Ipsum safdafafsaf',
+    progress: 39
+}]
 
-
-const UpcomingTask = ({ className }: { className?: string }) => {
+const UpcomingTask = ({ ...rest }) => {
     return (
-        <Card className={className}>
+        <Card {...rest}>
             <CardHeader>
                 <CardTitle className="text-base text-slate-700 dark:text-slate-200 font-extrabold">Upcoming Tasks</CardTitle>
             </CardHeader>
             <CardContent className="flex flex-col gap-3 overflow-hidden">
-                <Card className='pt-5'>
-                    <CardContent>
-                        <h3 className="font-bold text-sm text-slate-700">Create anassssssrwarwarw UI</h3>
-                        <h5 className="text-slate-500 text-[.750rem]">Lorem Ipsum safdafafsaf</h5>
-                    </CardContent>
-                    <CardFooter className="flex flex-col items-start gap-4">
-                        <div className="flex gap-2 items-center">
-                            <TbProgressCheck className="text-slate-600" size={23} />
-                            <h5 className="text-slate-600 text-[.750rem] font-semibold">Progress</h5>
-                        </div>
-                        <Progress  className="h-[.25rem]" value={50} />
-                        <Badge className="bg-blue-100 text-blue-700 py-1" variant="outline">25 Apr 2024</Badge>
-                    </CardFooter>
-                </Card>
-                <Card className='pt-5'>
-                    <CardContent>
-                        <h3 className="font-bold text-sm text-slate-700">Create anassssssrwarwarw UI</h3>
-                        <h5 className="text-slate-500 text-[.750rem]">Lorem Ipsum safdafafsaf</h5>
-                    </CardContent>
-                    <CardFooter className="flex flex-col items-start gap-4">
-                        <div className="flex gap-2 items-center">
-                            <TbProgressCheck className="text-slate-600" size={23} />
-                            <h5 className="text-slate-600 text-[.750rem] font-semibold">Progress</h5>
-                        </div>
-                        <Progress  className="h-[.25rem]" value={50} />
-                        <Badge className="bg-blue-100 text-blue-700 py-1" variant="outline">25 Apr 2024</Badge>
-                    </CardFooter>
-                </Card>
-                <Card className='pt-5'>
-                    <CardContent>
-                        <h3 className="font-bold text-sm text-slate-700">Create anassssssrwarwarw UI</h3>
-                        <h5 className="text-slate-500 text-[.750rem]">Lorem Ipsum safdafafsaf</h5>
-                    </CardContent>
-                    <CardFooter className="flex flex-col items-start gap-4">
-                        <div className="flex gap-2 items-center">
-                            <TbProgressCheck className="text-slate-600" size={23} />
-                            <h5 className="text-slate-600 text-[.750rem] font-semibold">Progress</h5>
-                        </div>
-                        <Progress  className="h-[.25rem]" value={50} />
-                        <Badge className="bg-blue-100 text-blue-700 py-1" variant="outline">25 Apr 2024</Badge>
-                    </CardFooter>
-                </Card>
-                <Card className='pt-5'>
-                    <CardContent>
-                        <h3 className="font-bold text-sm text-slate-700">Create anassssssrwarwarw UI</h3>
-                        <h5 className="text-slate-500 text-[.750rem]">Lorem Ipsum safdafafsaf</h5>
-                    </CardContent>
-                    <CardFooter className="flex flex-col items-start gap-4">
-                        <div className="flex gap-2 items-center">
-                            <TbProgressCheck className="text-slate-600" size={23} />
-                            <h5 className="text-slate-600 text-[.750rem] font-semibold">Progress</h5>
-                        </div>
-                        <Progress  className="h-[.25rem]" value={50} />
-                        <Badge className="bg-blue-100 text-blue-700 py-1" variant="outline">25 Apr 2024</Badge>
-                    </CardFooter>
-                </Card>
+
+                {
+                    data.map((content, index) => (<Card key={index} className='pt-5'>
+                        <CardContent>
+                            <h3 className="font-bold text-sm text-slate-700">{content.title}</h3>
+                            <h5 className="text-slate-500 text-[.750rem]">{content.description}</h5>
+                        </CardContent>
+                        <CardFooter className="flex flex-col items-start gap-4">
+                            <div className="flex justify-between w-full">
+                                <div className="flex items-center gap-2">
+                                    <TbProgressCheck className="text-slate-600" size={23} />
+                                    <h5 className="text-slate-600 text-[.750rem] font-semibold">Progress
+                                    </h5>
+                                </div>
+                               <Badge variant="outline">{`${content.progress}%`}</Badge>
+                              
+                            </div>
+                            <Progress className="h-[.25rem]" value={content.progress} />
+                            <Badge className="bg-blue-100 text-blue-700 py-1" variant="outline">25 Apr 2024</Badge>
+                        </CardFooter>
+                    </Card>))
+                }
+
             </CardContent>
-            
+
         </Card>
     )
 }
