b844c95875f30fb51c65674d68ea8c9de0a6b26b
unknown <rockph0029@gmail.com>
2024-04-18 16:43:01 +0800

    add side drawer

--- src/components/layout/nav-layout/Layout.tsx
+++ src/components/layout/nav-layout/Layout.tsx
@@ -4,25 +4,32 @@ import React from 'react'
 import { useTheme } from 'next-themes';
 import BreadcrumbsHelper from '../../ui helpers/breadcrumbs/BreadcrumbsHelper'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { MdOutlineDarkMode, MdLightMode } from "react-icons/md";
 import { usePathname } from 'next/navigation';
+
+import Drawer from '@/components/ui helpers/side-drawer/Drawer';
+
+import { MdOutlineDarkMode, MdLightMode } from "react-icons/md";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 
 
 
 const LayoutMain = ({ children }: { children: React.ReactNode }) => {
+
     const { theme, setTheme } = useTheme()
     const pathName = usePathname()
-    const route = pathName.split("/").filter(path=>path)
+    const route = pathName.split("/").filter(path => path)
 
-    const changeTheme = ()=>{
+    const changeTheme = () => {
         setTheme(theme === "light" ? "dark" : "light");
     }
     return (
         <section className="flex flex-col w-screen md:w-[calc(100vw-70px)] md:relative left-[58px] md:px-8 px-3 md:min-h-[calc(100vh+10px)]">
-            <header className="h-[75px] items-center flex justify-between">
+            <header className="h-[75px] items-center flex justify-between"> 
                 <BreadcrumbsHelper route={route} />
                 <div className="flex justify-center items-center gap-4">
+                    <Drawer>
+                        <RxHamburgerMenu className="block md:hidden" size={20} /> </Drawer>
                     <span className="cursor-pointer" onClick={changeTheme}>{theme === "dark" ? <MdLightMode size={22} /> : <MdOutlineDarkMode size={22} />}</span>
                     <Avatar>
                         <AvatarImage src="/man.svg" />
@@ -32,7 +39,7 @@ const LayoutMain = ({ children }: { children: React.ReactNode }) => {
             </header>
             {children}
         </section>
-    
+
     )
 }
 
