2c63ea49c29fb616c51463964cf53aed2637d1ea
unknown <rockph0029@gmail.com>
2024-05-06 15:58:17 +0800

    login page

--- src/auth.ts
+++ src/auth.ts
@@ -1,29 +1,27 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import Google from "next-auth/providers/google"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   session: {
     strategy: "jwt",
   },
   providers: [
-    Google
-    // Credentials({
-    //   credentials: {
-    //     name: {},
-    //     password: {},
-    //     auth: {},
-    //   },
-    //   authorize: async (credentials) => {
-    //     let user = { id: "", name: "", auth: "", email: "" };
-    //     user = {
-    //       id: "",
-    //       name: await credentials.name as string,
-    //       auth: "sample123",
-    //       email: "arwarwara@yahoo.com",
-    //     };
-    //     return user;
-    //   },
-    // }),
+    Credentials({
+      credentials: {
+        name: {},
+        password: {},
+        auth: {},
+      },
+      authorize: async (credentials) => {
+        let user = { id: "", name: "", auth: "", email: "" };
+        user = {
+          id: "",
+          name: await credentials.name as string,
+          auth: "sample123",
+          email: "arwarwara@yahoo.com",
+        };
+        return user;
+      },
+    }),
   ],
 });
