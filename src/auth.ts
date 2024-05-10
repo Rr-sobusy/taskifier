c409e3b101f02a351e5913a6dfc515aae853ce4b
unknown <rockph0029@gmail.com>
2024-05-10 16:50:51 +0800

    fix auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,27 +1,35 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import Google from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   session: {
     strategy: "jwt",
   },
+  adapter: PrismaAdapter(prisma),
   providers: [
-    Credentials({
-      credentials: {
-        name: {},
-        password: {},
-        auth: {},
-      },
-      authorize: async (credentials) => {
-        let user = { id: "", name: "", auth: "", email: "" };
-        user = {
-          id: "",
-          name: await credentials.name as string,
-          auth: "sample123",
-          email: "arwarwara@yahoo.com",
-        };
-        return user;
-      },
-    }),
+    // Credentials({
+    //   credentials: {
+    //     name: {},
+    //     password: {},
+    //     auth: {},
+    //   },
+    //   authorize: async (credentials) => {
+    //     let user = { id: "", name: "", auth: "", email: "" };
+    //     user = {
+    //       id: "",
+    //       name: await credentials.name as string,
+    //       auth: "sample123",
+    //       email: "arwarwara@yahoo.com",
+    //     };
+    //     return user;
+    //   },
+    // }),
+
+    Google
   ],
 });
